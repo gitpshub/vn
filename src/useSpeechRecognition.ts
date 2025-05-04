836eed94e6326b999f9966d634c712dddca34abe
Ed <mail@544062.ru>
2025-05-04 14:35:56 +0300

    feat: add error form - 2

--- src/useSpeechRecognition.ts
+++ src/useSpeechRecognition.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 declare global {
   interface Window {
@@ -20,7 +20,30 @@ export const useSpeechRecognition = (
   const [listening, setListening] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const recognitionRef = useRef<SpeechRecognition | null>(null);
+  const listeningRef = useRef<boolean>(false);
   const isAndroid = /Android/i.test(navigator.userAgent);
+
+  console.log('reload', listening);
+
+  const startListening = useCallback(() => {
+    console.log('startListening');
+    if (recognitionRef.current) {
+      // console.log(' - listening', listening);
+      setListening(true);
+      listeningRef.current = true;
+      recognitionRef.current.start();
+    }
+  },[]);
+
+  const stopListening = useCallback(() => {
+    console.log('stopListening', recognitionRef.current);
+    if (recognitionRef.current) {
+      // console.log(' - listening', listening);
+      setListening(false);
+      listeningRef.current = false;
+      recognitionRef.current.stop();
+    }
+  },[]);
 
   useEffect(() => {
     const SpeechRecognition =
@@ -37,47 +60,43 @@ export const useSpeechRecognition = (
     recognition.lang = 'ru-RU';
 
     recognition.onresult = (event: SpeechRecognitionEvent) => {
+      console.log('onresult', listening);
       const results = event.results[event.resultIndex];
       const transcript = results[0].transcript;
 
       if (results.isFinal && results[0].confidence > 0) {
         onFinalResult(transcript);
-        if (isAndroid) {
-          setTimeout(startListening, 100);
-        }
+        // if (isAndroid) {
+        //   setTimeout(startListening, 100);
+        // }
       } else {
         onInterimResult(transcript);
       }
     };
 
     recognition.onerror = (event: SpeechRecognitionErrorEvent) => {
+      console.log('onerror', event.error);
+      if (isAndroid && event.error == 'no-speech') {
+        setListening(true);
+        listeningRef.current = true;
+        return;
+      }
       setError(`${event.error}`);
     };
 
     recognition.onend = () => {
+      console.log('onend', listening, listeningRef.current);
       if (isAndroid) {
-        setTimeout(startListening, 100);
+        if (listeningRef.current) setTimeout(startListening, 100);
       } else {
         setListening(false);
+        listeningRef.current = false;
       }
     };
 
     return () => recognition.stop();
-  }, [onFinalResult, onInterimResult, isAndroid]);
+  }, [isAndroid, onFinalResult, onInterimResult, listening, startListening]); //isAndroid, onFinalResult, onInterimResult, listening
 
-  const startListening = () => {
-    if (recognitionRef.current) {
-      setListening(true);
-      recognitionRef.current.start();
-    }
-  };
-
-  const stopListening = () => {
-    if (recognitionRef.current) {
-      setListening(false);
-      recognitionRef.current.stop();
-    }
-  };
 
   return { listening, error, startListening, stopListening };
 };
