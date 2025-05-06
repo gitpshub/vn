97276b024dfc3a3086ad3355ca37154f94ab2e69
Ed <mail@544062.ru>
2025-05-06 15:23:53 +0300

    feat: add draft timer refactor - 4

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useCallback } from 'react';
 import { FaPlay, FaPause, FaTimes } from 'react-icons/fa';
 
 const Timer = () => {
-  // Состояния
   const [isTimerRun, setIsTimerRun] = useState(() => {
     const savedStatus = localStorage.getItem('timerRunning');
     return savedStatus ? JSON.parse(savedStatus) : false;
@@ -13,7 +12,6 @@ const Timer = () => {
     return savedTime ? parseInt(savedTime, 10) : null;
   });
 
-  // Сохраняем состояние в localStorage
   useEffect(() => {
     localStorage.setItem('timerRunning', JSON.stringify(isTimerRun));
   }, [isTimerRun]);
@@ -26,21 +24,19 @@ const Timer = () => {
     }
   }, [startTime]);
 
-  // Вычисляем прошедшее время
   const getElapsedTime = useCallback((start: number | null) => {
     if (!start) return 0;
     const now = Date.now();
     return now - start;
-  },[]);
+  }, []);
 
   const [elapsedTime, setElapsedTime] = useState(getElapsedTime(startTime));
 
-  // Обновляем время каждую секунду
   useEffect(() => {
     let interval = undefined;
 
     if (isTimerRun) {
-      setElapsedTime(getElapsedTime(startTime)); // сразу обновляем
+      setElapsedTime(getElapsedTime(startTime));
       interval = setInterval(() => {
         setElapsedTime(getElapsedTime(startTime));
       }, 1000);
@@ -49,7 +45,26 @@ const Timer = () => {
     return () => clearInterval(interval);
   }, [isTimerRun, getElapsedTime, startTime]);
 
-  // Форматируем время с разными стилями для mm:ss
+  useEffect(() => {
+    const vibrateInterval = 60 * 1;
+
+    if (!isTimerRun || !('vibrate' in navigator)) return;
+
+    const totalSeconds = Math.floor(elapsedTime / 1000);
+    const lastVibrationTime = parseInt(
+      localStorage.getItem('lastVibrationTime') || '0'
+    );
+
+    if (
+      totalSeconds >= vibrateInterval &&
+      totalSeconds % vibrateInterval === 0 &&
+      totalSeconds !== lastVibrationTime
+    ) {
+      navigator.vibrate([200, 100, 200]); // трель
+      localStorage.setItem('lastVibrationTime', totalSeconds.toString());
+    }
+  }, [elapsedTime, isTimerRun]);
+
   const formatTime = (ms: number) => {
     const totalSeconds = Math.floor(ms / 1000);
     const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, '0');
@@ -59,31 +74,51 @@ const Timer = () => {
     );
     const seconds = String(totalSeconds % 60).padStart(2, '0');
 
+    const color = isTimerRun ? '#333' : '#999';
+
     return (
       <>
-        <span style={{ fontSize: '24px', fontWeight: 'bold', color: '#333' }}>{`${hours}:${minutes}`}</span>
-        <span style={{ fontSize: '16px', fontWeight: 'bold', color: '#999', marginLeft: '4px' }}>{seconds}</span>
+        <span
+          style={{ fontSize: '24px', fontWeight: 'bold', color: `${color}` }}
+        >
+          {`${hours}:${minutes}`}
+        </span>
+        <span
+          style={{
+            fontSize: '16px',
+            fontWeight: 'bold',
+            color: '#999',
+            marginLeft: '4px',
+          }}
+        >
+          {seconds}
+        </span>
       </>
     );
   };
 
   const formatRoundedTime = (ms: number) => {
-    const totalMinutes = Math.ceil((ms / 1000 / 60) / 15) * 15;
+    const totalMinutes = Math.ceil(ms / 1000 / 60 / 15) * 15;
     const hours = String(Math.floor(totalMinutes / 60)).padStart(2, '0');
     const minutes = String(totalMinutes % 60).padStart(2, '0');
-  
+
     return (
-      <span style={{ fontSize: '24px', fontWeight: 'bold', color: '#999', marginLeft: 'auto' }}>
+      <span
+        style={{
+          fontSize: '24px',
+          fontWeight: 'bold',
+          color: '#999',
+          marginLeft: 'auto',
+        }}
+      >
         {hours}:{minutes}
       </span>
     );
   };
 
-  // Управление таймером
   const toggleTimer = () => {
     if (!isTimerRun) {
-      const now = Date.now();
-      setStartTime(now);
+      setStartTime(Date.now());
       setIsTimerRun(true);
     } else {
       setIsTimerRun(false);
@@ -95,45 +130,41 @@ const Timer = () => {
     setIsTimerRun(false);
     setStartTime(null);
     setElapsedTime(0);
+    localStorage.removeItem('lastVibrationTime');
   };
 
   return (
     <div
       className='timer'
-      style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: '10px',
+      }}
     >
       <button
         className={isTimerRun ? 'animate' : ''}
         onClick={toggleTimer}
         style={{
           backgroundColor: isTimerRun ? '#ff4444' : '#4CAF50',
-        //   border: 'none',
-        //   padding: '8px',
-        //   borderRadius: '4px',
         }}
       >
         {isTimerRun ? <FaPause /> : <FaPlay />}
       </button>
-
-      {/* Отображение времени с разными цветами */}
-      <span
-        style={{ userSelect: 'none' }}
-      >
-        {formatTime(elapsedTime)}
-      </span>
+      <span style={{ userSelect: 'none' }}>{formatTime(elapsedTime)}</span>
 
       <span
         style={{ fontSize: '18px', fontWeight: 'bold', userSelect: 'none' }}
       >
         {formatRoundedTime(elapsedTime)}
       </span>
-      
+
       <button
         onClick={resetTimer}
         style={{
           backgroundColor: '#4CAF50',
-        //   marginLeft: 'auto'
-         }}
+        }}
       >
         <FaTimes />
       </button>
